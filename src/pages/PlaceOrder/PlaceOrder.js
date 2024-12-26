695939ae22e322b359ad012ac6b146a25cd7be14
DareIsrael <dareisrael4@gmail.com>
2024-12-26 10:14:20 -0800

    few adj

--- src/pages/PlaceOrder/PlaceOrder.js
+++ src/pages/PlaceOrder/PlaceOrder.js
@@ -44,7 +44,9 @@ const PlaceOrder = () => {
         amount: getTotalCartAmount() + 2
     }
 
-    let response = await axios.post(url+"/api/order/place", orderData, { headers: {token} })
+    let response = await axios.post(url+"/api/order/place", orderData, { headers: {
+        Authorization: `Bearer ${token}`
+    }  })
     if (response.data.success) {
         const {session_url} = response.data;
         window.location.replace(session_url);
